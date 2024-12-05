d1490708b41e27e68a2e09eacae52de1a0695da5
Nathaniel Horn <132115518+Amlis-Sanches@users.noreply.github.com>
2024-12-04 21:35:17 -0600

    adding changes for sessions.

--- Coding Projects/CS50’s Web Programming with Python and JavaScript/Week 3 Django/lecture3/tasks/views.py
+++ Coding Projects/CS50’s Web Programming with Python and JavaScript/Week 3 Django/lecture3/tasks/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 
-#sample tasks
-tasks = ["foo","bar","baz"]
+
+class NewTaskForm(forms.Form):
+    tasks = forms.CharField(label="New Task")
+    #priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 def index(request)->render:
     """_summary_
@@ -11,6 +17,25 @@ def index(request)->render:
     Args:
         request (_type_): Django requirment
     """
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["tasks"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html",{
+                "form": form
+            })
+            
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
---
+++ HTML Notes/Class Notes/CS50 HTML and CSS/Week 3/Tasks Example Program.md
@@ -0,0 +1,4 @@
+Here we are creating an example app with Django for a page that will display all the available tasks. Here we will outline how to create this app from beginning to end in detail incase we miss anything.
+
+# Create The App
+First
